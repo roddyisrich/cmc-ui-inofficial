12c6aa87fc265c07127807676f85f8692c5ab923
roddyisrich <burrowsroderick1@gmail.com>
2025-09-07 15:46:28 +0000

    Add MarkSackerberg working mint functionality

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,22 @@
 import type { AppProps } from "next/app"
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets'
+import { clusterApiUrl } from '@solana/web3.js'
+import { useMemo } from 'react'
+import '@solana/wallet-adapter-react-ui/styles.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const endpoint = useMemo(() => clusterApiUrl('devnet'), [])
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], [])
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  )
 }
---
+++ utils/UmiProvider.tsx
@@ -0,0 +1,31 @@
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import { walletAdapterIdentity } from "@metaplex-foundation/umi-signer-wallet-adapters";
+import { mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { ReactNode } from "react";
+import { UmiContext } from "./useUmi";
+import { mplCandyMachine } from  "@metaplex-foundation/mpl-core-candy-machine"
+import { createNoopSigner, publicKey, signerIdentity } from "@metaplex-foundation/umi";
+import { dasApi } from '@metaplex-foundation/digital-asset-standard-api';
+
+export const UmiProvider = ({
+  endpoint,
+  children,
+}: {
+  endpoint: string;
+  children: ReactNode;
+}) => {
+  const wallet = useWallet();
+  const umi = createUmi(endpoint)
+  .use(mplTokenMetadata())
+  .use(mplCandyMachine())
+  .use(dasApi())
+  if (wallet.publicKey === null) {
+    const noopSigner = createNoopSigner(publicKey("11111111111111111111111111111111"))
+    umi.use(signerIdentity(noopSigner));
+  } else {
+    umi.use(walletAdapterIdentity(wallet))
+  }
+
+  return <UmiContext.Provider value={{ umi }}>{children}</UmiContext.Provider>;
+};
