294eb545eb3eb30781311950e810940d5d65d3aa
roddyisrich <burrowsroderick1@gmail.com>
2025-09-07 15:19:35 +0000

    Fix broken _app.tsx, basic working version

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,36 +1,5 @@
-import { AppProps } from 'next/app';
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
-import { clusterApiUrl } from '@solana/web3.js';
-import { useMemo } from 'react';
-import { UmiProvider } from '../utils/UmiProvider';
-import '@solana/wallet-adapter-react-ui/styles.css';
+import type { AppProps } from "next/app"
 
-function MyApp({ Component, pageProps }: AppProps) {
-  const network = WalletAdapterNetwork.Devnet;
-  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-
-  const wallets = useMemo(
-    () => [
-      new PhantomWalletAdapter(),
-      new SolflareWalletAdapter(),
-    ],
-    []
-  );
-
-  return (
-    <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets} autoConnect>
-        <WalletModalProvider>
-          <UmiProvider>
-            <Component {...pageProps} />
-          </UmiProvider>
-        </WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider>
-  );
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
-
-export default MyApp;
